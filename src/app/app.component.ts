cce37f1883cca7ca795234c1564a6274ace8254f
MatheusSantos2003 <111252177+MatheusSantos2003@users.noreply.github.com>
2023-01-30 21:28:42 -0300

    final initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,9 +19,20 @@ export class AppComponent {
     { descricao: 'Fobias'},
     { descricao: 'Morte e luto'},
     { descricao: 'Orientação Profissional'},
-    { descricao: 'TOC'}
+    { descricao: 'Transtorno Obsessivo Compulsivo - TOC'}
 
   ]
 
+  whatsappClick(){
+    window.open('https://wa.me/+5511948081340/?text=Ola%2C%20vim%20pelo%20seu%20site%2C%20e%20gostaria%20de%20marcar%20uma%20consulta%20ou%20conhecer%20mais%20sobre%20seus%20servicos!', '_blank');
+  }
+
+  scrollTo(id:string){
+    const element = document.getElementById(id);
+
+    if(element){
+      element.scrollIntoView({behavior: 'smooth'})
+    }
+  }
 
 }
